1a0d16752e09abc6758b6889faffd66e7a7f5db6
rozsavolgyi <simonrozsavolgyi5@gmail.com>
2024-11-26 09:56:36 +0100

    first commit

---
+++ models/Restaurant.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const restaurantsSchema = new mongoose.Schema({
+    address: {
+        building: String,
+        coord: [Number],
+        street: String,
+        zipcode: String,
+    },
+    borough: {
+        type: String,
+        required: true
+    },
+    cuisine: {
+        type: String,
+        required: true
+    },
+//     grades: {
+//         type: Array,
+//         required: true
+//     },
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     restaurant_id: {
+//         type: String,
+//         required: true
+//     }
+});
+
+module.exports = mongoose.model("Restaurant", restaurantsSchema);
---
+++ routes/Restaurantsroutes.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const Model = require("../models/Restaurant")
+
+router.get('/', async (req, res) => {
+    try {
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.post('/', async (req, res) => {
+    const newUser = new Model(req.body);
+    try {
+        const dataToSave = await newUser.save();
+        res.status(201).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+module.exports = router;
